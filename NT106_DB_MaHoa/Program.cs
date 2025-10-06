6af2cd2cf0d2fdb7f11b0d773c7bf9271bbe2051
_thnhngaan_ <nganthanh368108@gmail.com>
2025-10-06 14:20:38 +0700

    Database

---
+++ NT106_DB_MaHoa/Program.cs
@@ -0,0 +1,14 @@
+namespace NT106_2._2
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1()); // ??m b?o tên Form ?úng
+        }
+    }
+
+}
