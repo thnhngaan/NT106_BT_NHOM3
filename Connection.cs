647105fb7d071e5d946b8f605e74057865fafa63
_thnhngaan_ <nganthanh368108@gmail.com>
2025-10-07 22:45:27 +0700

    Add files via upload

---
+++ Connection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace NT106_2._2
+{
+    internal class Connection
+    {
+        private static string stringConnection = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\DELL\source\repos\NT106_2.2\Database1.mdf;Integrated Security=True;Connect Timeout=30;Encrypt=True";
+        public static SqlConnection GetSqlConnection()
+        {
+            return new SqlConnection(stringConnection);
+        }
+    }
+}
